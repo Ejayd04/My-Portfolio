65145d0cf4180f56c2e38c0d25f840919fc0c4ae
Ejoke Ekpagha <ejay@Ejokes-MacBook-Air.local>
2024-01-24 23:41:04 -0500

    Commit all updates including deleted files and newly created file

--- js/main.js
+++ js/main.js
@@ -1,48 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    function onMenuClick() {
-        let navbar = document.getElementById("navbar");
-        navbar.classList.toggle("show");
-    }
-});
 
-function isInViewport(element) {
-    let rect = element.getBoundingClientRect();
-    return rect.top >= 0 && rect.bottom <= window.innerHeight;
+let menubar = document.querySelector(".menu-bar");
+let introSection = document.querySelector(".intro-section");
+
+menubar.onclick = function () {
+    let navBar = document.querySelector(".right-header");
+    navBar.classList.toggle("active");
+    introSection.classList.toggle("active");
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    let offerSection = document.getElementById("offer-section");
-
-
-    function addFadeInClass() {
-        if (isInViewport(offerSection) && !offerSection.classList.contains("fade-in")) {
-            offerSection.classList.add("fade-in");
-        } else if (!isInViewport(offerSection) && offerSection.classList.contains("fade-in")) {
-            offerSection.classList.remove("fade-in");
-        }
-    }
-
-    window.addEventListener("scroll", function () {
-        addFadeInClass();
-    });
-
-    addFadeInClass();
-});
-
-const peculiarItems = document.querySelectorAll('.peculiar-item');
-const firstImage = peculiarItems[0].querySelector('img');
-const secondImage = peculiarItems[3].querySelector('img');
-
-function handleScroll() {
-    if (isInViewport(firstImage)) {
-        firstImage.style.transform = 'translateX(0)'; // Slide in from the left
-      }
-    
-      if (isInViewport(secondImage)) {
-        secondImage.style.transform = 'translateX(0)'; // Slide in from the right
-      }
-}
-
-window.addEventListener('scroll', handleScroll);
-
-handleScroll();
